7f1ec27c46ca0d0db8b49523429245674a148ce3
Seth Harden <sethmh82@users.noreply.github.com>
2017-09-05 16:56:30 -0700

    done

---
+++ load.js
@@ -0,0 +1,17 @@
+PUBLIC SITE		
+https://www.southstatebank.com/internet/wp-admin/	
+zUOvd9oiW%%I9WI61kHusbOm	
+seth
+
+https://dev.southstatebank.com/internet/wp-admin	
+Q6Puwvfi*6Wgjv*byfZx1msb	
+sharden@inkrit.com
+
+INTRANET SITE		
+https://testintranet.southstatebank.com/intranet/wp-admin/	
+D*xtb0diiu6u*%RAs%H^!Sf^	
+sharden
+
+https://southstateintranet.southstatebank.com/wp-login.php	
+WG3@95qF9mF%V!&gcBdS#psn	
+sharden
